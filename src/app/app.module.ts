9a98ce4a7c30540892ae317f2c507bc0566c6c10
sarasavi <gayanlumbini@gmail.com>
2019-06-14 13:55:37 +0530

    home added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GayanComponent } from './gayan/gayan.component';
 import { HelloComponent } from './hello/hello.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GayanComponent,
-    HelloComponent
+    HelloComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
